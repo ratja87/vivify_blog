c7b428304b9392486c74994f821c8218b242ad6f
ratja87 <ratja87@gmail.com>
2018-01-31 19:08:17 +0100

    zadatak 2

---
+++ comments.php
@@ -0,0 +1,7 @@
+
+<?php include 'header.php';?>
+
+
+<?php include 'sidebar.php';?>
+<?php include 'footer.php';?>
+
---
+++ create-post.php
@@ -0,0 +1,8 @@
+
+
+include 'footer.php';
+
+
+<?php include 'sidebar.php'
+<?php include 'footer.php'
+
